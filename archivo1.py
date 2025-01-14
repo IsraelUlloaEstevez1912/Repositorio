b4ee61d65b3cae25f9b95c4bb638795aa7dab998
Israel Ulloa Estevez <israelulloa46@gmail.com>
2025-01-13 23:26:55 -0400

    Agregando comentatios al codigo

--- archivo1.py
+++ archivo1.py
@@ -1,9 +1,11 @@
+# funcion para sumar dos numeros
 def suma(n1,n2):
     return n1 + n2
 
-print(suma(5,6))
-
+# funcion para restar dos numeros
 def resta(n1,n2):
     return n1 - n2
 
+# imprimimos resultados
+print(suma(5,6))
 print(resta(5,6))
